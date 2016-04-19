723f58c6d2433b7702154e40203e3eab77f02be9
Alejandro Terrazas <Alex@MacBook-Pro-de-Alejandro.local>
2016-04-19 08:15:19 -0500

    :rocket: Create Users

--- MyBlog/db/schema.rb
+++ MyBlog/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160418181754) do
+ActiveRecord::Schema.define(version: 20160419125704) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20160418181754) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "picture"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
